f70bda99eca6e92e4b632c48c27ba256ad1330d4
Gabriel Ong <ojh809@gmail.com>
2022-10-24 17:47:14 +0800

    Added FSPL method to calculate distance given RSSI Value

--- prototype1/src/entity/Person.java
+++ prototype1/src/entity/Person.java
@@ -1,6 +1,7 @@
 package entity;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -24,14 +25,21 @@ public class Person {
 
         for (Map<String, Double> m : rssiValues) {
             Set<String> ids = m.keySet();
+            List<Map<Router, Double>> routerList = new ArrayList<>();
 
             // Assign RSSI values to routers
             for (String id : ids) {
+                Map<Router, Double> routerMap = new HashMap<>();
                 Router router = ap.map.getRouter(id);
                 if (router == null) {
                     return;
                 }
                 router.setRSSI(m.get(id));
+                
+                // Calculate approximate distance to router and store in map + add to ArrayList
+                double distance = this.getEstimatedDistanceWithRSSIValue(router);
+                routerMap.put(router, distance);
+                routerList.add(routerMap);
             }
 
             // Triangulation Formula
@@ -40,7 +48,17 @@ public class Person {
              * - Generate {x, y}
              * - Store into locations arraylist
              */
+
+             
         }
+    }
+
+    private double getEstimatedDistanceWithRSSIValue(Router router) {
+        double freqInMHz = router.getFrequency();
+        double levelInDb = router.getRssi();
+
+        double exp = (27.55 - (20 * Math.log10(freqInMHz)) + Math.abs(levelInDb)) / 20.0;
+        return Math.pow(10.0, exp);
     }
 
     public void renderPerson(Graphics2D g2) {
