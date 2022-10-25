90dbca3bd192333710f073b330159975095c686e
Gabriel Ong <ojh809@gmail.com>
2022-10-25 21:57:10 +0800

    Added Trilaterion Formula

--- prototype1/src/entity/Person.java
+++ prototype1/src/entity/Person.java
@@ -4,9 +4,14 @@ import java.awt.Font;
 import java.awt.Graphics2D;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.Map.Entry;
+
+import org.ejml.data.*;
+import org.ejml.simple.SimpleMatrix;
 
 import main.AppPanel;
 
@@ -50,6 +55,57 @@ public class Person {
                 routerList.add(routerMap);
             }
 
+            SimpleMatrix matrixA = null;
+            SimpleMatrix matrixB = null;
+            int nX = 0;
+            int nY = 0;
+            Double nDistance = 0.0;
+
+            // Get nth element in ArrayList
+            Map<Router, Double> nMap = routerList.remove(routerList.size() - 1);
+            for (Router r : nMap.keySet()) {
+                nDistance = nMap.get(r);
+                nX = r.getX();
+                nY = r.getY();
+            }
+
+            // Get rest of the elements and fill up matrix A and B
+            Iterator<Entry<Router, Double>> setIter = nMap.entrySet().iterator();
+            while (setIter.hasNext()) {
+                // Getting all the variables
+                Entry<Router, Double> entry = setIter.next();
+                Router r = entry.getKey();
+                Double tempDistance = entry.getValue();
+                int tempX = r.getX();
+                int tempY = r.getY();
+
+                SimpleMatrix tempARow = new SimpleMatrix(new double[][]{ {2 * (tempX - nX), 2 * (tempY - nY)} } );
+                SimpleMatrix tempBRow = new SimpleMatrix(new double[][]{ {Math.pow(tempX, 2) + Math.pow(tempY, 2) - Math.pow(nX, 2) - Math.pow(nY, 2) - Math.pow(tempDistance, 2) + Math.pow(nDistance, 2)} } );
+
+                if (matrixA == null) {
+                    matrixA = tempARow;
+                } else {
+                    matrixA.concatRows(tempARow);
+                }
+
+                if (matrixB == null) {
+                    matrixB = tempBRow;
+                } else {
+                    matrixB.concatRows(tempBRow);
+                }
+            }
+
+            SimpleMatrix matrixATransposed = matrixA.transpose();
+            SimpleMatrix projectionMatrix = ((matrixA.mult(matrixATransposed)).invert()).mult(matrixATransposed);
+
+            SimpleMatrix resultMatrix = projectionMatrix.mult(matrixB);
+
+            int xCoord = (int) resultMatrix.get(0, 0);
+            int yCoord = (int) resultMatrix.get(1, 0);
+
+
+            locations.add(new int[]{xCoord, yCoord});
+            
             // Triangulation Formula
             /*
              * TODO: INSERT FORMULA HERE
